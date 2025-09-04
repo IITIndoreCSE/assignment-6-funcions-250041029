8c3918a20b5f7cee98d3c8295c3039c4dd75b859
250041029 <mc250041029@iiti.ac.in>
2025-09-04 09:55:00 +0530

    Update prime_palindrome.cpp

--- prime_palindrome.cpp
+++ prime_palindrome.cpp
@@ -1,24 +1,36 @@
 #include <iostream>
 using namespace std;
 
+// Function to check if a number is prime
 bool isPrime(int n) {
-    // TODO: complete the function as per instructions
-
+    if (n <= 1) return false;
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) return false;
+    }
+    return true;
 }
 
+// Function to check if a number is palindrome
 bool isPalindrome(int n) {
-    // TODO: complete the function as per instructions
-
+    int original = n, reversed = 0;
+    while (n > 0) {
+        reversed = reversed * 10 + (n % 10);
+        n /= 10;
+    }
+    return original == reversed;
 }
 
+// Function to check if a number is both prime and palindrome
 bool primePalindrome(int n) {
-    // TODO: complete the function as per instructions
-    
+    return isPrime(n) && isPalindrome(n);
 }
 
 int main() {
-    int n; cin >> n;
+    int n;
+    cin >> n;
+
     cout << (primePalindrome(n) ? 1 : 0) << "\n";
+
     return 0;
 }
-
+	
